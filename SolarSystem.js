14a6f6ef2cdaaa7238a088f2b93e0604824813dc
Loïck Marin <marin.loick@gmail.com>
2023-11-24 09:55:42 +0100

    test API en cours

---
+++ SolarSystem.js
@@ -0,0 +1,9 @@
+fetch("SolarSystem.json")
+  .then( response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then( json => initialize(json) )
+  .catch( err => console.error(`Fetch problem: ${err.message}`) );
