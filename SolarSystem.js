0462e642ea028c4812d0b97d92df577f7172c8ac
EdithMenechi <edith.menechi@gmail.com>
2023-11-27 10:13:59 +0100

    positionnement rajouté

--- SolarSystem.js
+++ SolarSystem.js
@@ -32,17 +32,3 @@ window.onload = function() {
 };
 
 
-
-
-
-
-
-fetch("SolarSystem.json")
-  .then( response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then( json => initialize(json) )
-  .catch( err => console.error(`Fetch problem: ${err.message}`) );
