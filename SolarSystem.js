a46ba7c8b49a5f8c32cd8ca4d618b717ee949d45
Loïck Marin <marin.loick@gmail.com>
2023-11-27 09:52:13 +0100

    appel API terminé

--- SolarSystem.js
+++ SolarSystem.js
@@ -1,9 +1,45 @@
-fetch("SolarSystem.json")
-  .then( response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then( json => initialize(json) )
-  .catch( err => console.error(`Fetch problem: ${err.message}`) );
+document.addEventListener("DOMContentLoaded", () => {
+  const planetsContainer = document.getElementById("planets-container");
+
+  // Récupère les données de l'API
+  fetch("https://api.le-systeme-solaire.net/rest/bodies/")
+    .then(response => response.json())
+    .then(data => {
+      const planets = data.bodies.filter(body => body.isPlanet && body.englishName !== "Sun");
+
+      // Crée des éléments HTML pour chaque planète
+      planets.forEach(planet => {
+        const newPlanet = document.createElement("div");
+        newPlanet.className = "planet";
+        newPlanet.style.backgroundColor = getRandomColor();
+        newPlanet.style.left = `${Math.random() * 80}vw`; // Position aléatoire simple test
+        newPlanet.style.top = `${Math.random() * 80}vh`; // Position aléatoire simple test
+        newPlanet.title = planet.englishName;
+        newPlanet.onclick = () => displayPlanetInfo(planet);
+
+        planetsContainer.appendChild(newPlanet);
+      });
+
+      // Ajoute le Soleil
+      const sun = document.createElement("div");
+      sun.id = "sun";
+      sun.style.left = "40vw"; // Position du Soleil
+      sun.style.top = "40vh"; // Position du Soleil
+      sun.title = "Sun";
+      sun.onclick = () => displayPlanetInfo(data.bodies.find(body => body.englishName === "Sun"));
+
+      planetsContainer.appendChild(sun);
+    })
+    .catch(error => console.error("Erreur de chargement des données :", error));
+
+  // Fonction pour afficher les informations d'une planète qu'on pourra changer + ajout d'infos
+  function displayPlanetInfo(planet) {
+    alert(`Nom : ${planet.englishName}\nMasse : ${planet.mass.massValue} ${planet.mass.massExponent}`);
+    // Personnalisation des infos (pas d'idée pour faire plus court)
+  }
+
+  // Fonction pour générer une couleur aléatoire simple test
+  function getRandomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+});
